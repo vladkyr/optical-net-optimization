44df97580395a5f117a579e16a10f69f7bb03784
Vladyslav Kyryk <vkyryk@deloittece.com>
2022-05-26 09:51:16 +0200

    repo restructured

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import importGraph
 import revealGraph
 
-rel_path = 'ger50_1m-1y/demandMatrix-germany50-DFN-1month-200401.txt'
+rel_path = '../data/ger50_1m-1y/demandMatrix-germany50-DFN-1month-200401.txt'
 rows_to_skip = 19, 83
 network_nodes = 50      # 69-20+1
 network_demands = 2186 - 84 + 1  # 2186-84+1
